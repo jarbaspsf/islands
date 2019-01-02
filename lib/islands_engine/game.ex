dd83c09524572b20b30890a0a0e87e6ffc36a4be
Jarbas Silva Filho <jarbas.filho@esig.com.br>
2019-01-02 14:10:22 -0300

    GenServer call and cast, first steps

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -5,4 +5,20 @@ defmodule IslandsEngine.Game do
     IO.puts "This message has been handdled by handle_info/2, matching on :first"
     {:noreply, state}
   end
+
+  def handle_call(:demo_call, from, state) do
+      {:reply, state, state}
+  end
+
+  def demo_call(game) do
+    GenServer.call(game, :demo_call)
+  end
+
+  def handle_cast({:demo_cast, new_value}, state) do
+    {:noreply, Map.put(state, :test, new_value)}
+  end  
+
+  def demo_call(game, new_value) do
+    GenServer.cast(game, {:demo_cast, new_value})
+  end
 end
